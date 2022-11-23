de48c34d8981607bf00c8bf43d49ba9b0ca82f8f
midhunsoftwareassociates <midhun@softwareassociates.in>
2022-11-23 15:38:10 +0530

    uniquePlant program added

---
+++ uniquePlant.py
@@ -0,0 +1,34 @@
+import random
+
+f = open("plants.txt")
+a = []
+for p in f:
+    a.append(p)
+
+name = input('Enter your full name: ').lower()
+
+plants = {}
+name_array = name.split()
+l = len(name_array)
+
+
+def find_plant(name, i):
+    for n in name:
+        plants[n] = list()
+        for p in a:
+            if n == p[0]:
+                plants[n].append(p)
+            else:
+                continue
+        if len(plants[n]) != 0:
+            break
+        else:
+            del plants[n]
+            continue
+
+
+for i in range(l):
+    find_plant(name_array[i], i)
+
+for e in plants:
+    print(random.choice(plants[e]), end=' ')
